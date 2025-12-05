3673b59692734df3cd122f455330340bf96be409
KinoCrp <kino.crp@gmail.com>
2025-12-06 07:07:02 +0800

    Update Logging

--- module/src/main/cpp/log.h
+++ module/src/main/cpp/log.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <android/log.h>
-
+#ifdef DEBUG
 #define LOG_TAG "Kinocrp"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#else
+#define LOGD(...) do{}while(0)
+#define LOGW(...) do{}while(0)
+#define LOGE(...) do{}while(0)
+#define LOGI(...) do{}while(0)
+#endif
