63d791b8a2152250e4c910f81f854d2ab23ce1ba
KinoCrp <kino.crp@gmail.com>
2026-03-23 17:33:44 +0800

    Update

---
+++ module/src/main/cpp/loader.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stddef.h>
+
+void hack_prepare(const char *game_data_dir, void *data, size_t length);
---
+++ module/src/main/cpp/service/service.cpp
@@ -0,0 +1,3 @@
+#include "service.h"
+
+extern "C" volatile __attribute__((used)) int v0 = 0;
